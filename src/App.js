467f0ebbe8b152186d3220d9e9929e29f51b4962
AhmedHolbertonTUN <jarrayaahmed119@gmail.com>
2020-09-06 23:23:21 +0100

    API checkpoint

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+import Userlist from "./UserList";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h2>List Of Users</h2>
+      <Userlist />
+    </div>
+  );
+}
